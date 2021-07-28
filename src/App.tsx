74a8e8fbb6368eba6e04b4b1fd883603abb2a5ae
romulorocha08 <romulosilva0819@gmail.com>
2021-07-28 03:01:47 -0300

    styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
- export function App() {
+import styled from 'styled-components'
+
+const Title = styled.h1 `
+font-size: 64px;
+color: #8257e6;
+`
+
+export function App() {
   return (
     <div className="App">
-     <h1>hello world</h1>
+    <Title>Hello world</Title> 
     </div>
   );
 }
