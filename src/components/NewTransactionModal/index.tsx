43f1d473ec2761e377c179ffd361c4f5a56a8ca9
romulorocha08 <romulosilva0819@gmail.com>
2021-08-11 04:11:49 -0300

    estilizando modal 3

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
 import Modal from 'react-modal';     
+import VectorImg from '../../assets/vector.svg'
 import { Container } from './style';
 
 interface NewTransactionModalProps {
@@ -14,6 +15,16 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
       overlayClassName="react-modal-overlay"
       className="react-modal-content"
      >
+       
+       <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
+       >
+
+         <img src={VectorImg} alt="fechar modal" />
+       </button>
+
        <Container>
          <h2>Cadastrar transação</h2>
 
@@ -31,7 +42,7 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
          />
 
          <button type="submit">
-           Cadastrar valor
+           Cadastrar 
          </button>
          
        </Container>
--- src/components/NewTransactionModal/style.ts
+++ src/components/NewTransactionModal/style.ts
@@ -40,6 +40,10 @@ export const Container = styled.form`
     border: 0;
     font-size: 1rem;
     margin-top: 1.5rem;
+    font-weight: 600;
+    
+    transition: filter 0.2s;
+
 
     &:hover {
       filter: brightness(0.9)
